b4b3c7e987542fe312e1f34369eb20357f9a8261
dschiese <schiesedennis@googlemail.com>
2024-03-28 10:52:16 +0100

    Completed workflow

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 const process = require('process');
 const fs = require('fs');
 
-let envVariables = process.env.map(key => key); // stores the env variable names
+let envVariableNames = Object.keys(process.env).map(key => key); // stores the env variable names
 
 // Change dir to runner home dir
 try {
@@ -16,11 +16,31 @@ let envFilePaths = findAllEnvFiles();
 
 core.setOutput("files", envFilePaths);
 
+envFilePaths.forEach(envFile => {
+    replaceEnvs(envFile);
+})
+
+/**
+ * Replaces all replaceable variables with the existing env vars
+ * @param file path to .env-file
+ */
+function replaceEnvs(file) {
+    fs.readFile(file, 'utf-8', function (err, data) {
+        let replaced = data;
+        envVariableNames.forEach(envVariable => {
+            replaced.includes(envVariable) ? replaced = replaced.replace(envVariable, process.env[envVariable]) : null;
+        })
+        fs.writeFile(file, replaced, function (err) {
+            if (err) return console.log(err);
+        });
+    });
+}
+
 function findAllEnvFiles() {
     let foundEnvFiles = []
     const files = fs.readdirSync(".", { recursive: true }); // reads the contents of the directory
-    for(file in files) {
+    files.forEach(file => {
         file.endsWith(".env") ? foundEnvFiles.push(file) : null;
-    }
+    })
     return foundEnvFiles;
 }
